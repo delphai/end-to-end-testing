47729f61e0b439df31d377ce62867d340c91d45f
Jean Yang <jean@delphai.com>
2020-08-12 16:16:56 +0200

    try to test filter

--- delphaiTesting.ts
+++ delphaiTesting.ts
@@ -4,9 +4,9 @@ const puppeteer = require('puppeteer');
 try {
     (async () => {
         const browser = await puppeteer.launch({ headless: false});
-        // console.info(browser);
         const page = await browser.newPage();
 
+        // login
         await page.setViewport({ width: 1280, height: 800 })
         await page.goto('https://app.delphai.com')
         await page.type( '[type=text]', 'jean@delphai.com')
@@ -20,6 +20,25 @@ try {
         await page.click('.anticon-search')
         await page.waitForSelector('.main-container');
         await page.screenshot({ path: 'searchResult.png' })
+
+        // open fliter 
+        await page.click('[type=button]')
+        await page.waitForSelector('ant-slider-handle')
+
+
+        // hover
+        // await page.hover('.ant-slider-handle')
+        // await page.screenshot({ path: 'hover.png' })
+        // slider
+        const e = await page.$('ant-slider-handle')
+        const box = await e.boundingBox();
+        
+        await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
+        await page.mouse.down("right");
+        await page.mouse.move(0, 100); // move to (100, 200) coordinates
+        await page.mouse.up();
+
+        await page.screenshot({ path: 'save.png' })
 
         await browser.close()
     })();
