2be59c138f4db311dae4d7908d3a0c4abfd5c719
Jean Yang <jean@delphai.com>
2020-08-17 13:35:21 +0200

    test search func in the fliter

--- delphaiTesting.ts
+++ delphaiTesting.ts
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 
 try {
     (async () => {
-        const browser = await puppeteer.launch({ headless: false});
+        const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
 
         // login
@@ -25,23 +25,13 @@ try {
         await page.click('[type=button]')
         await page.waitForSelector('.filters-container')
 
-        
-        // slider
-        // const e = await page.$('[role=slider]')
-        // const box = await e.boundingBox();
-        
-        // await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
-        // await page.mouse.down("right");
-        // await page.mouse.move(0, 100); // move to (100, 200) coordinates
-        // await page.mouse.up();
+        // select a dropdown from the fliter
+        await page.click('.ant-select-selector')
+        await page.waitForSelector('.ant-select-selection-search')
 
-        // Drags the mouse from a point
-        // await page.mouse.move(0, 0);
-        // await page.mouse.down();
-        
-        // // Drops the mouse to another point
-        // await page.mouse.move(100, 100);
-        // await page.mouse.up();
+        // search in the fliter
+        await page.type('.ant-select-selection-search-input', 'aisa')
+        await page.waitForSelector('.ant-select-selection-item')
 
 
         await page.screenshot({ path: 'save.png' })
