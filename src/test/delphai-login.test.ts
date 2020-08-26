7ed14ac5faaa9e4cb92684c1997c0bcf63bce474
Jean Yang <jean@delphai.com>
2020-08-26 14:09:56 +0200

    can open filter

--- src/test/delphai-login.test.ts
+++ src/test/delphai-login.test.ts
@@ -18,19 +18,13 @@ describe('test delphai search', () => {
         await page.type('[type=text]', 'green house')
         await page.click('.d-component-search-icon')
         const searchResult = await page.waitForSelector('.main-container');
-        // const ss = await page.screenshot({ path: 'searchResult.png' })
         expect(searchResult).toBeTruthy
     }, timeout)
 
-    // test('should open filters', async () => {
-    //     // await page.waitForXPath('//button[@class="ant-btn toolbar-filter-btn text-font-style ant-btn-primary"]')
-    //     // const element = await page.$x('//button[@class="ant-btn toolbar-filter-btn text-font-style ant-btn-primary"]')
-    //     // const element = await page.$x('//button[@type="button"]')
-    //     // console.log(element)
-    //     // await element[1].click()
-    //     await page.click('[type=button]')
-    //     const searchDropdown = await page.waitForSelector('.filters')
-    //     expect(searchDropdown).toBeTruthy
-    // }, timeout)
+    test('should open filters', async () => {
+        await page.click('[type=button]')
+        const searchDropdown = await page.waitForSelector('.filters')
+        expect(searchDropdown).toBeTruthy
+    }, timeout)
 })
 
